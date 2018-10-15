a34c3a8b316f1fedc47e3dc8fc1713dcedb8f30c
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-10-15 19:51:06 -0400

    Empty methods created!

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,5 +1,26 @@
 public class RationalNumber extends RealNumber {
 
+//private int a;
+//private int b;
 
+public RationalNumber() {
+
+
+}
+
+public int getRational() {
+
+	return 0;
+}
+
+public String toString() {
+
+	return "";
+} 
+
+public String CompareReal(int x, int y) {
+
+	return "";
+}
 
 }
--- RealNumber.java
+++ RealNumber.java
@@ -1,5 +1,25 @@
 public class RealNumber {
 
+private double x;
 
+public RealNumber() {
+
+
+}
+
+public double getValue() {
+
+	return x;
+}
+
+public String toString() {
+
+	return "";
+}
+
+public String CompareReal(double a, double b) {
+
+	return "";
+}
 
 }
